a3c5dc799907640aa7d6a81a9184d6d1ac765521
Michael Dekoski <michaeldekoski@gmail.com>
2023-06-09 15:48:04 -0400

    Assignment 9

--- C Assignments/ECE 101 Assignment 9 Michael Dekoski.c
+++ C Assignments/ECE 101 Assignment 9 Michael Dekoski.c
@@ -21,78 +21,83 @@ int initFirst, waitFirst, timeFirst, dispFirst = 0;
 int totalTime, savedTime = -1;
 
 int main() {
-    while (1) {
-        if (currentState == 0) { // Shutdown State
-            printf("Quitting.\n");
-            break;
-        } else if (currentState == 1) { // Initial State
-            if (!initFirst) { // First time in this state
-                printf("I\n");
-                startTime = clock();
-                initFirst = 1;
-            } else {
-                stopTime = clock();
-                totalTime = stopTime - startTime;
-            }
-            if (totalTime > 1000) {
-                currentState = 2; // Goto Wait State
-            }
-        } else if (currentState == 2) { // Wait State
-            if (!waitFirst) { // First time in this state
-                printf("W\n");
-                waitFirst = 1;
-            }
-            if (GetAsyncKeyState(VK_CONTROL)) {
-                getch();
-                currentState = 3; // Goto Timing State
-            } else if (GetAsyncKeyState(VK_ESCAPE)) {
-                currentState = 0; // Goto Shutdown State
-            } else if (GetAsyncKeyState('B')) { // B Shows the saved time
-                if (savedTime == -1) {
-                    printf("--\n");
+    while(1) {
+        switch(currentState) {
+            case 0:
+                printf("Shutting Down\n");
+                Sleep(2000);
+                return 0;
+            case 1:
+                if (!initFirst) { // First time in this state
+                    printf("I\n");
+                    startTime = clock();
+                    initFirst = 1;
                 } else {
-                    stopwatch(savedTime);
+                    stopTime = clock();
+                    totalTime = stopTime - startTime;
                 }
-            }
-        } else if (currentState == 3) { // Timing State
-            if (!timeFirst) { // First time in this state
-                printf("T\n");
-                startTime = clock();
-                timeFirst = 1;
-            }
-            if (GetAsyncKeyState(VK_CONTROL)) {
-                getch();
-                currentState = 4; // Goto Disp State
-                stopTime = clock();
-                totalTime = stopTime - startTime;
-            } else if (GetAsyncKeyState(VK_ESCAPE)) {
-                currentState = 0; // Goto Shutdown State
-            } else if (GetAsyncKeyState('B')) {
-                if (savedTime == -1) {
-                    printf("--\n");
+                if (totalTime > 1000) {
+                    currentState = 2; // Goto Wait State
+                }
+                break;
+            case 2:
+                if (!waitFirst) { // First time in this state
+                    printf("W\n");
+                    waitFirst = 1;
+                }
+                if (GetAsyncKeyState(VK_CONTROL)) {
+                    getch();
+                    currentState = 3; // Goto Timing State
+                } else if (GetAsyncKeyState(VK_ESCAPE)) {
+                    currentState = 0; // Goto Shutdown State
+                } else if (GetAsyncKeyState('B')) { // B Shows the saved time
+                    if (savedTime == -1) {
+                        printf("--\n");
+                    } else {
+                        stopwatch(savedTime);
+                    }
+                }
+                break;
+            case 3:
+                if (!timeFirst) { // First time in this state
+                    printf("T\n");
+                    startTime = clock();
+                    timeFirst = 1;
+                }
+                if (GetAsyncKeyState(VK_CONTROL)) {
+                    getch();
+                    currentState = 4; // Goto Disp State
+                    stopTime = clock();
+                    totalTime = stopTime - startTime;
+                } else if (GetAsyncKeyState(VK_ESCAPE)) {
+                    currentState = 0; // Goto Shutdown State
+                } else if (GetAsyncKeyState('B')) {
+                    if (savedTime == -1) {
+                        printf("--\n");
+                    } else {
+                        stopwatch(savedTime);
+                    }
+                }
+                break;
+            case 4:
+                if (!dispFirst) { // First time in this state
+                    stopwatch(totalTime);
+                    startTime = clock();
+                    dispFirst = 1;
                 } else {
-                    stopwatch(savedTime);
+                    stopTime = clock();
+                    totalTime = stopTime - startTime;
                 }
-            }
-        } else if (currentState == 4) { // Disp State
-            if (!dispFirst) { // First time in this state
-                stopwatch(totalTime);
-                startTime = clock();
-                dispFirst = 1;
-            } else {
-                stopTime = clock();
-                totalTime = stopTime - startTime;
-            }
-            if (totalTime > 1000) {
-                currentState = 2; // Goto Wait State
-                waitFirst = 0;
-                timeFirst = 0;
-                dispFirst = 0;
-            }
+                if (totalTime > 1000) {
+                    currentState = 2; // Goto Wait State
+                    waitFirst = 0;
+                    timeFirst = 0;
+                    dispFirst = 0;
+                }
+                break;
         }
         Sleep(100);
     }
-    return 0;
 }
 
 void stopwatch(int totalTimeW) {
